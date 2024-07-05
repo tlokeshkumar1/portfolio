e09501ac0cbc3d8cc3066b87f50e5e0f4c02a5cd
Joe Sixpack <lokeshkumaetelagamalla@g_mail.com>
2024-07-05 18:46:05 +0530

    initial

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    const currentLocation = location.href;
+    const menuItem = document.querySelectorAll('nav ul li a');
+    const menuLength = menuItem.length;
+    for (let i = 0; i < menuLength; i++) {
+        if (menuItem[i].href === currentLocation) {
+            menuItem[i].className = 'active';
+        }
+    }
+});
